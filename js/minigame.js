8074e6a894e8a9d75b7c9a2f74575f777860920e
Jacorb <39597610+Jacorb90@users.noreply.github.com>
2021-06-24 12:13:13 -0400

    NGP3C Beta 27 Update

--- js/minigame.js
+++ js/minigame.js
@@ -7,7 +7,9 @@ function getStartPlayer() { return {
 	started: false,
 	pts: new Decimal(0),
 	upgs: [null, new Decimal(0), new Decimal(0), new Decimal(0), new Decimal(0)],
+	autoUpgs: false,
 	prestige: new Decimal(0),
+	prestigeUpgs: [null, new Decimal(0), new Decimal(0)],
 }}
 
 function loadMinigameStuff() {
@@ -37,16 +39,23 @@ function save() { localStorage.setItem(lsTitle, btoa(JSON.stringify(player))) }
 function gameLoop(diff) {
 	updateTemp();
 	
-	player.pts = player.pts.root(player.upgs[1].max(1)).plus(player.upgs[1].times(Decimal.pow(2, player.upgs[2]).times(Decimal.pow(OVERALL_DATA.totalScore, player.upgs[4]))).pow(tmp.mainExp).times(diff)).pow(player.upgs[1].max(1));
+	player.pts = player.pts.root(player.upgs[1].times(tmp.upgPower).max(1)).plus(player.upgs[1].times(tmp.upgPower).times(Decimal.pow(2, player.upgs[2].times(tmp.upgPower)).times(Decimal.pow(OVERALL_DATA.totalScore, player.upgs[4].times(tmp.upgPower)))).pow(tmp.mainExp).times(diff)).pow(player.upgs[1].times(tmp.upgPower).max(1));
+	if (player.autoUpgs && (player.prestige.gte(1e6)||player.prestigeUpgs[1].gte(12))) maxAll();
 }
 
 function updateTemp() {
-	tmp.mainExp = Decimal.add(Math.log(OVERALL_DATA.totalScore+1)+1, 1).times(Decimal.mul(player.upgs[3], 0.25).plus(1));
-	tmp.prestigeGain = player.pts.plus(1).log("1e12375").root(1.2).floor();
-	tmp.nextPrestige = Decimal.pow("1e12375", tmp.prestigeGain.plus(1).pow(1.2));
+	tmp.upgPower = player.prestigeUpgs[1].div(4).plus(1);
+	tmp.mainExp = Decimal.add(Math.log(OVERALL_DATA.totalScore+1)+1, 1).times(Decimal.mul(player.upgs[3].times(tmp.upgPower), 0.25).plus(1)).times(player.pts.plus(1).log10().plus(1).log2().times(player.prestigeUpgs[2]).div(100).plus(1));
+	tmp.prestigeGain = player.pts.plus(1).log("1e12375").root(1.2)
+	if (tmp.prestigeGain.gte(50)) tmp.prestigeGain = Decimal.pow(50, tmp.prestigeGain.log(50).sqrt());
+	tmp.prestigeGain = tmp.prestigeGain.times(Decimal.pow(2, player.prestigeUpgs[1])).times(player.pts.plus(1).log10().plus(1).log2().times(player.prestigeUpgs[2]).div(100).plus(1)).floor();
+	
+	let p2 = tmp.prestigeGain.plus(1).div(Decimal.pow(2, player.prestigeUpgs[1])).div(player.pts.plus(1).log10().plus(1).log2().times(player.prestigeUpgs[2]).div(100).plus(1));
+	if (p2.gte(50)) p2 = Decimal.pow(50, p2.log(50).pow(2));
+	tmp.nextPrestige = Decimal.pow("1e12375", p2.pow(1.2));
 
 	let p = player.prestige;
-	tmp.prestigeEff = p.plus(1).log2().pow(1.1);
+	tmp.prestigeEff = p.plus(1).log2().pow(1.1).times(player.pts.plus(1).log10().plus(1).log2().times(player.prestigeUpgs[2]).div(100).plus(1));
 }
 
 function getUpgCost(x) {
@@ -79,11 +88,26 @@ function doPrestige() {
 }
 
 function maxAll() {
-	if (player.prestige.gte(10)) {
+	if (player.prestige.gte(10)||player.prestigeUpgs[1].gte(1)) {
 		for (let i=1;i<=4;i++) {
 			if (i>1) if (player.upgs[i-1].lt(1)) return;
 			if (player.pts.lt(getUpgCost(i))) continue;
 			player.upgs[i] = player.upgs[i].max(getUpgTarg(i));
 		}
 	}
+}
+
+let startPrestigeUpgCosts = [null, 100, 5e8]
+
+function getPrestigeUpgCost(x) {
+	let cost = Decimal.pow(9+x, Decimal.pow(1.3+0.1*x, player.prestigeUpgs[x].div(x).pow(.4+x/10)).sub(1)).times(startPrestigeUpgCosts[x]);
+	if (cost.gte(25*startPrestigeUpgCosts[x])) cost = Decimal.pow(25*startPrestigeUpgCosts[x], cost.log(25*startPrestigeUpgCosts[x]).pow(3))
+	return cost;
+}
+
+function buyPrestigeUpg(x) {
+	let cost = getPrestigeUpgCost(x);
+	if (player.prestige.lt(cost)) return;
+	player.prestige = player.prestige.sub(cost)
+	player.prestigeUpgs[x] = player.prestigeUpgs[x].plus(1);
 }
