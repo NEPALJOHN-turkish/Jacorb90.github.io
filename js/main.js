08e6aea2695b587a9ba547925d70d2ab7c9185d0
Jacorb <39597610+Jacorb90@users.noreply.github.com>
2021-08-05 07:31:17 -0400

    UE:R 4

--- js/main.js
+++ js/main.js
@@ -158,17 +158,17 @@ const GAME_DATA = {
 		url: "https://jacorb90.me/UE-Rewritten/",
 		player: localStorage.getItem("UErewritten")?JSON.parse(atob(localStorage.getItem("UErewritten"))):undefined,
 		lib: Decimal,
-		endgame: new Decimal(145),
+		endgame: new Decimal(817),
 		score() {
 			let data = this.player;
 			if (!data) return 0;
 
 			let s = 0;
-			s += new Decimal(data.depth||0).div(this.endgame).sqrt().min(1).times(54).toNumber()
+			s += new Decimal(data.depth||0).div(this.endgame).cbrt().min(1).times(57).toNumber()
 			if (isNaN(s)) s = 0;
 			return Math.min(Math.floor(s), this.scoreLimit);
 		},
-		scoreLimit: 54,
+		scoreLimit: 57,
 	},
 	ptc: {
 		id: "ptc",
